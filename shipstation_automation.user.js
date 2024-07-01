bc3602d6e4fbc2833fc04fab10ddece6db2ef7d1
peter <peter@vapewholesaleusa.com>
2024-07-01 09:02:18 -0700

    update version

--- shipstation_automation.user.js
+++ shipstation_automation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shipstation proglo automation
 // @namespace    http://tampermonkey.net/
-// @version      1.0.6
+// @version      1.0.7
 // @description  Automate shipstation Proglo workflow
 // @author       Peter Chen
 // @match        https://progloshipping.com/*
